715bc004688e3d9edba63e2ec105769ba686e9b1
Dmitry Bor <dembic587@gmail.com>
2024-11-20 10:08:53 +0300

    Update player.gd

--- snake/player.gd
+++ snake/player.gd
@@ -6,7 +6,8 @@ func  get_input():
 	var input_direction = Input.get_vector("player_left", "player_right", "player_up","player_down")
 	velocity += input_direction * Global.speed
 	var label = get_node("label")
-	label.update_text("Ha")
+	#label.update_text("Ha")
+	print(velocity)
 		
 func _physics_process(_delta):
 	get_input()
